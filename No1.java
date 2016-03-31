94ba2f50bbef3329c903bae061d09e714d83d68b
Muhammad Avriza Bramantyo <mavrizab@gmail.com>
2016-03-31 10:19:58 +0700

    Added files via upload

---
+++ No1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no1;
+
+import java.util.Scanner;
+public class No1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=0,b=1,hasil=0,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Masukkan Deret Fibonacci : ");
+        c=sc.nextInt();
+        for(int i=1;i<=c;i++){
+            a=b;
+            b=hasil;
+            hasil=hasil+a;
+            System.out.println(hasil+" ");
+        }
+    }
+    
+}
